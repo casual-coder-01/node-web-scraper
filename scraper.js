60a9bb4eb83e8bab5b10f717640a0c70a3dde1dd
Abhinav Thakur <abhinav2006206@gmail.com>
2026-01-21 12:54:22 +0530

    Scrape book titles and prices and save to JSON

--- scraper.js
+++ scraper.js
@@ -1,19 +1,31 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
+
 
 const URL = "https://books.toscrape.com/";
 
 async function scrapeBooks() {
-  console.log("Loading HTML into Cheerio...");
-
   const response = await axios.get(URL);
   const html = response.data;
-
   const $ = cheerio.load(html);
-  const books = $(".product_pod");
-console.log("Books found on page:", books.length);
 
+  const books = [];
+
+  $(".product_pod").each((index, element) => {
+    const title = $(element).find("h3 a").attr("title");
+    const price = $(element).find(".price_color").text();
+
+    books.push({
+      title,
+      price,
+    });
+  });
+
+  console.log(books);
+
+  fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+  console.log("Data saved to books.json");
 }
-
 scrapeBooks();
 
